ebeb7a9477649d896b68718530672d2790c58ccc
smallbee <smallbee3@gmail.com>
2018-03-19 15:19:23 +0900

    Snippet Pagination function done

--- app/snippets/apis/generic.py
+++ app/snippets/apis/generic.py
@@ -1,4 +1,5 @@
 from rest_framework import mixins, generics, permissions, renderers
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 
 from snippets.permissions import IsOwnerOrReadOnly
@@ -6,9 +7,17 @@ from ..serializers import SnippetSerializer
 from ..models import Snippet
 
 
+class StandardResultsSetPagination(PageNumberPagination):
+    page_size = 3
+    page_size_query_param = 'page_size'
+    max_page_size = 5
+
+
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+
+    pagination_class = StandardResultsSetPagination
 
     permission_classes = (
         permissions.IsAuthenticatedOrReadOnly,
